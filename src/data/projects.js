0d2498427e000449bf1ec9f9ac95031c93abd2c2
callme-edward <edwy23@gmail.com>
2020-06-04 18:31:39 +0530

    a lot of done

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id : 1,
+        title: 'Portfolio React Application',
+        description: 'A simple portfolio app developed with reactjs and simple web dev concepts',
+        link: 'https://github.com/callme-edward/reactjs-portfolio',
+        image: project1
+    },
+
+    {
+        id : 2,
+        title: 'My API',
+        description: 'A REST api built with nodejs from scratch',
+        link: 'https://github.com/callme-edward/node_crud',
+        image: project2
+    },
+
+    {
+        id : 3,
+        title: 'An E-Commerce Shop',
+        description: 'An E-Commerce Shop built with nodejs mongodb and espress',
+        link: 'https://github.com/callme-edward/Book_store_JS',
+        image: project3
+    }
+
+];
+
+export default PROJECTS; 
---
+++ src/data/socialProfile.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import hackrankIcon from '../assets/hackrank_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id : 1,
+        link : 'mailto:edwy23@gmail.com',
+        image: emailIcon
+    },
+    {
+        id : 2,
+        link : 'https://github.com/callme-edward',
+        image: githubIcon
+    },
+    {
+        id : 3,
+        link : 'https://www.linkedin.com/in/rahul-sagar-006278141/',
+        image: linkedinIcon
+    },
+    {
+        id : 4,
+        link : 'https://www.hackerrank.com/edwy23',
+        image: hackrankIcon
+    }
+]
+
+export default SOCIAL_PROFILES;
